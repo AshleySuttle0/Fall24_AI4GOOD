f243f0dcead41773ca657a50b9b9a307c1ec82e1
AshleySuttle0 <ashy@uoregon.edu>
2024-12-04 17:28:07 -0800

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -1,2 +1,27 @@
 def test_load():
   return 'loaded'
+
+def compute_probs(neg,pos):
+  p0 = neg/(neg+pos)
+  p1 = pos/(neg+pos)
+  return [p0,p1]
+
+def cond_prob(full_table, the_evidence_column, the_evidence_column_value, the_target_column, the_target_column_value):
+  assert the_evidence_column in full_table
+  assert the_target_column in full_table
+  assert the_evidence_column_value in up_get_column(full_table, the_evidence_column)
+  assert the_target_column_value in up_get_column(full_table, the_target_column)
+
+def cond_probs_product(full_table, evidence_row, target_column, target_column_value):
+  assert target_column in full_table
+  assert target_column_value in up_get_column(full_table, target_column)
+  assert isinstance(evidence_row, list)
+  assert len(evidence_row) == len(up_list_column_names(full_table)) - 1   # - 1 because subtracting off the target column from full_table
+def prior_prob(full_table, the_column, the_column_value):
+  assert the_column in full_table
+  assert the_column_value in up_get_column(full_table, the_column)
+def naive_bayes(full_table, evidence_row, target_column):
+  assert target_column in full_table
+  assert isinstance(evidence_row, list)
+  assert len(evidence_row) == len(up_list_column_names(full_table)) - 1   # - 1 because subtracting off the target
+
