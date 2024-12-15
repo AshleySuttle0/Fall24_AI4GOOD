49458fdec0ca83f5e888bcdfb8c0f3b63488c37d
AshleySuttle0 <ashy@uoregon.edu>
2024-12-14 21:00:46 -0800

    my_library.py

--- my_library.py
+++ my_library.py
@@ -63,3 +63,46 @@ def naive_bayes(full_table, evidence_row, target_column):
   #return your 2 results in a list
   return [neg, pos]
 
+from sklearn.ensemble import RandomForestClassifier
+
+def run_random_forest(train, test, target, n):
+  #target is target column name
+  #n is number of trees to use
+
+  assert target in train   #have not dropped it yet
+  assert target in test
+
+  #your code below - copy, paste and align from above
+
+  from sklearn.ensemble import RandomForestClassifier
+  clf = RandomForestClassifier(n_estimators=n, max_depth=2, random_state=0)
+
+  X = up_drop_column(train, target)
+  y = up_get_column(train,target)
+  assert isinstance(y,list)
+  assert len(y)==len(X)
+
+  clf.fit(X, y)  #builds the trees in the forest - saves you hours of work by hand
+
+  k_feature_table = up_drop_column(test, target)
+  k_actuals = up_get_column(test, target)
+
+  probs = clf.predict_proba(k_feature_table)  #Note no need here to transform k_feature_table to list - we can just use the table. Nice.
+
+  assert len(probs)==len(k_actuals)
+  assert len(probs[0])==2
+
+  pos_probs = [p for n,p in probs]  #just the positive probabilities
+
+  all_mets = []
+  for t in thresholds:
+    predictions = [1 if pos>t else 0 for pos in pos_probs]
+    pred_act_list = up_zip_lists(predictions, k_actuals)
+    mets = metrics(pred_act_list)
+    mets['Threshold'] = t
+    all_mets = all_mets + [mets]
+
+  metrics_table = up_metrics_table(all_mets)
+  metrics_table
+  return metrics_table
+
