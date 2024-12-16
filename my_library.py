453ebed7bf711173042aab92a324ab15865fa8a3
AshleySuttle0 <ashy@uoregon.edu>
2024-12-15 19:00:15 -0800

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -1,5 +1,9 @@
 def test_load():
   return 'loaded'
+def compute_probs(neg,pos):
+  p0 = neg/(neg+pos)
+  p1 = pos/(neg+pos)
+  return [p0,p1]
 def cond_prob(full_table, the_evidence_column, the_evidence_column_value, the_target_column, the_target_column_value):
   assert the_evidence_column in full_table
   assert the_target_column in full_table
